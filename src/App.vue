6509b79b209a6945faafce2d5823a0061e5b07bb
Or Bracha <orbracha50@gmail.com>
2024-08-21 14:16:33 +0300

    first upload(skelton toy index)

---
+++ src/App.vue
@@ -0,0 +1,31 @@
+<script setup>
+import { RouterLink, RouterView } from "vue-router";
+</script>
+
+<template>
+  <header>
+    <nav>
+      <RouterLink to="/">Home</RouterLink>
+      <RouterLink to="/about">About</RouterLink>
+    </nav>
+  </header>
+
+<RouterView />
+</template>
+
+<style  scoped>
+header {
+  position: absolute;
+  top: 0;
+ left: 5px;
+ right: 5px;
+  width: 98vw;
+  padding: 10px 5px;
+  margin-right: 5px;
+  margin-top: 5px;
+  background-color: orange;
+}
+body{
+  background-color: lightgray;
+}
+</style>
---
+++ src/pages/About.vue
@@ -0,0 +1,15 @@
+<template>
+  <p>
+    Lorem ipsum dolor, sit amet consectetur adipisicing elit. Sapiente vitae corporis illum a nobis quibusdam laudantium ea expedita sed sint dolores velit reprehenderit consequatur iusto, saepe possimus soluta. Quo, nam?
+  </p>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
